d3405107c343c7240c9b8f91ad8204187cad5559
Munira <Munirabs33@gmail.com>
2026-08-03 10:41:56 +0300

    Initial Commit

---
+++ AWAN/AWANApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWANApp.swift
+//  AWAN
+//
+//  Created by munirah alsubaie on 20/02/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct AWANApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AWAN/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AWAN
+//
+//  Created by munirah alsubaie on 20/02/1448 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
