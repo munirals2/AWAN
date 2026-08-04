e4af83ebf250a06f6f74bce0d6315d02050a1a53
Norah Almulhim <norahalmulhim@10-101-41-220.0101.twq.sa.iosda.org>
2026-08-04 09:42:17 +0300

    main page comit

--- AWAN/SwiftUIView.swift
+++ AWAN/SwiftUIView.swift
@@ -10,16 +10,48 @@ import SwiftUI
 struct SwiftUIView: View {
     var body: some View {
         ZStack{
-            Image("back").resizable().scaledToFill().ignoresSafeArea()
-            VStack{
+            Image("back")
+                            .resizable()
+                            .scaledToFill()
+                            .ignoresSafeArea()
+            VStack(spacing: 2) {
+
+                Text("أوَانْ")
+                    .font(.system(size: 70))
+                    .foregroundStyle(.blue)
+                    .bold()
+
+                Image("OLD")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 420, height: 420)
+
+                Text("Never Miss\nYour Medication")
+                    .font(.system(size: 36))
+                    .fontWeight(.bold)
+                    .foregroundStyle(.blue)
+                    .multilineTextAlignment(.center)
+
+                Text("We'll remind you at the right time so you never forget your medicine.")
+                    .font(.system(size: 16))
+                    .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                HStack(spacing: 10){
+                    Circle()
+                        .fill(Color.blue)
+                        .frame(width: 10, height: 10)
+                    Circle()
+                        .fill(Color.blue.opacity(0.3))
+                        .frame(width: 10, height: 10)
+                    Circle()
+                        .fill(Color.blue.opacity(0.3))
+                        .frame(width: 10, height: 10)
+                }
+
                 
-                
-                Text("أوَانْ").font(.system(size: 50)).foregroundStyle(.blue).bold()
-                Spacer()
-                
-                Image("OLD").resizable().scaledToFit().frame(width: 500, height: 500).padding(.bottom, 350).offset(y: -20)
             }
-            .padding(.top, 100)
+            .padding(.top, 60)
         }
     }
 }
