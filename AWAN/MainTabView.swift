81c656d6796936b7769e6b2ecb9003c7dc0253d2
Munira <Munirabs33@gmail.com>
2026-08-09 09:43:22 +0300

    calander interface final

--- AWAN/MainTabView.swift
+++ AWAN/MainTabView.swift
@@ -1,34 +1,33 @@
-//
-//  MainTabView.swift
-//  AWAN
-//
-//  Created by Tumadhir Alyahya on 22/02/1448 AH.
-//
-
 import SwiftUI
 
 struct MainTabView: View {
     var body: some View {
         TabView {
-            
-            HomeView()
-                .tabItem {
-                    Image(systemName: "square.stack.3d.up")
-                    Text("List")
-                }
+            NavigationView {
+                HomeView()
+            }
+            .tabItem {
+                Image(systemName: "square.stack.3d.up")
+                Text("List")
+            }
 
-            myapp()
-                .tabItem {
-                    Image(systemName: "calendar")
-                    Text("Appointments")
-                }
+            NavigationView {
+                myapp()
+            }
+            .tabItem {
+                Image(systemName: "calendar")
+                Text("Appointments")
+            }
 
-            MyMedicineView()
-                .tabItem {
-                    Image(systemName: "pill")
-                    Text("Medications")
-                }
+            NavigationView {
+                MyMedicineView()
+            }
+            .tabItem {
+                Image(systemName: "pill")
+                Text("Medications")
+            }
         }
+        .navigationViewStyle(.stack) // optional but recommended for iPad
     }
 }
 
