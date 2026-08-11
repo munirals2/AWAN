171763a57b6154cc0e237117dcf61e85948b21b9
retaj <retajabdullah7ii@gmail.com>
2026-08-11 10:00:21 +0300

    merge fourth page with team changes

---
+++ awanwidget/awanwidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  awanwidgetBundle.swift
+//  awanwidget
+//
+//  Created by rataj abdullah aldebeebi on 28/02/1448 AH.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct awanwidgetBundle: WidgetBundle {
+    var body: some Widget {
+        awanwidget()
+        awanwidgetControl()
+        awanwidgetLiveActivity()
+    }
+}
